3256c9ff74bd9b0a2af52c46ba647174dbcec65a
Zoë Blade <zoe@bytenoise.co.uk>
2015-11-07 17:46:20 +0000

    Start work on .bin renamer

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
-all:
+bin-rename:
+	gcc bin-rename.c -o bin-rename
+	./bin-rename sonic-2.bin
+
+smd-to-bin:
 	gcc smd-to-bin.c -o smd-to-bin
 	./smd-to-bin sonic-2.smd
---
+++ bin-rename.c
@@ -0,0 +1,45 @@
+/*
+ * Mega Drive / Genesis ROM renamer
+ * by Zoe Blade, 2015
+ *
+ * Usage: "bin-rename *.bin *.BIN"
+ *
+ * Based heavily on Bart Trzynadlowski's GROM Version 0.75
+ * Public domain
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define KB 1024
+
+int main(int argc, char *argv[])
+{
+	FILE      *inputFilePointer;
+	FILE      *outputFilePointer;
+	char      outputFilename[40] = "out.bin\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"; /* Up to 40 chars: 32 for the filename, 1 space, 3 for the regions, 1 dot, 3 for the extension. */
+	int16_t   byte;
+	int16_t   byteNumber;
+
+	if (argc == 1) {
+		return 0; /* Only work with named files, not stdin */
+	} else {
+		while (--argc > 0) {
+			/* Loop through each file */
+			inputFilePointer = fopen(*++argv, "rb");
+
+			if (inputFilePointer == NULL) {
+				continue;
+			}
+
+			fseek(inputFilePointer, 512, SEEK_SET);
+			byteNumber = 0;
+
+			fclose(inputFilePointer);
+			rename (*argv, outputFilename);
+			putc('\n', stdout);
+		}
+	}
+
+	return 0;
+}
