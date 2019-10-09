b3235cb83d3a1f74c02c2877b92fe7710336dd25
Zoë Blade <zoe@bytenoise.co.uk>
2019-10-09 11:16:25 +0100

    Start to parse XML
    As per https://docs.python.org/2/library/xml.etree.elementtree.html

--- mamerename.py
+++ mamerename.py
@@ -1,6 +1,13 @@
+#!/usr/bin/python
+
 # MAME Rename
 # For Python 3
-# By Zoe Blade, 2019
+# By Zoe Blade, 2019
 
 # Warns which ROM files need to be renamed or replaced
 # for the given version of MAME
+
+import xml.etree.ElementTree as ET
+tree = ET.parse('mame.xml')
+root = tree.getroot()
+exit()
