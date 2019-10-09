c9ba0625f319c772cc24b4b9263cb383d9c5321c
Zoë Blade <zoe@bytenoise.co.uk>
2019-10-09 11:50:29 +0100

    Compare SHA1s

--- mamerename.py
+++ mamerename.py
@@ -7,26 +7,36 @@
 # Warns which ROM files need to be renamed or replaced
 # for the given version of MAME
 
+import hashlib # For calculating SHA1 digests
+import os # For directory listings
 import xml.etree.ElementTree as ET
-import os
 
 # Parse XML file
 tree = ET.parse('mame.xml')
 root = tree.getroot()
 
 # For each machine in the roms dir...
-for ourMachine in os.listdir('roms'):
-	print ourMachine
+for ourMachineName in os.listdir('roms'):
+	print ourMachineName
 
 	# Try to find its match...
 	for theirMachine in root.iter('machine'):
-		if theirMachine.get('name') != ourMachine:
+		if theirMachine.get('name') != ourMachineName:
 			continue
 
 		theirRoms = theirMachine.iter('rom')
-		ourRoms = os.listdir(os.path.join('roms', ourMachine))
-		print(theirRoms)
-		print(ourRoms)
-		exit()
+		for ourRomName in os.listdir(os.path.join('roms', ourMachineName)):
+			print ' ' + ourRomName
+			romFile = open(os.path.join('roms', ourMachineName, ourRomName), 'rb')
+			romData = romFile.read()
+			ourHash = hashlib.sha1(romData).hexdigest()
+			print(ourHash)
 
+			for theirRom in theirRoms:
+				if theirRom.get('sha1') != ourHash:
+					continue
+
+				theirRomName = theirRom.get('name')
+				print(ourRomName + ' ' + theirRomName)
+				exit()
 exit()
